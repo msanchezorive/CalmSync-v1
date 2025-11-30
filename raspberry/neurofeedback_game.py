e6039f7dc06bbb5ab1e3d46210a1102665c52775
Mónica Sánchez Orive <m.sanchezorive@gmail.com>
2025-11-30 22:31:37 +0100

    changed assets path in neurofeedback

--- raspberry/neurofeedback_game.py
+++ raspberry/neurofeedback_game.py
@@ -14,7 +14,8 @@ from udp import EEGClient
 # CONSTANTES GENERALES / CONFIG
 # ---------------------------------------------------------------------
 
-ASSETS_PATH = 'assets'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ASSETS_PATH = os.path.join(BASE_DIR, 'assets')
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 FPS = 30
